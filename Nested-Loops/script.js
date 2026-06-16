9bd788620e148f3f2d1e79e392c926525bd90901
Saakhavat-Prime <sakhavathussayn@gmail.com>
2026-06-17 00:09:42 +0500

    NestedLoops

---
+++ Nested-Loops/script.js
@@ -0,0 +1,25 @@
+let names = [["ali", "ahmed", "hassan"], ["sara", "ayesha", "jack"], ["67", "45", "90"]];
+// console.log(names);
+
+for (i = 0; i < names.length; i++) {
+    console.log(names[i])
+
+    for (j = 0; j < names[i].length; j++) {
+    console.log(names[i][j])
+    }
+}
+
+
+
+
+// Nested for of loop 
+
+let numbers = [["30", "40", "50"],["60", "70", "80"],["90", "10", "20"]];
+
+
+for (number of numbers){
+    console.log(number);
+    for(num of number){
+        console.log(num);
+    }
+}
