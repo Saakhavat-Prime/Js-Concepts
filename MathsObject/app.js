d7cb189e2e6bc6b7aa649c5a4f7b8973c1709226
Saakhavat-Prime <sakhavathussayn@gmail.com>
2026-06-19 22:04:15 +0500

    maths

---
+++ MathsObject/app.js
@@ -0,0 +1,24 @@
+Math.abs(12)  //without sign
+
+Math.power(2,5);
+
+Math.floor(4.9);   //4 smallest integer value
+
+Math.ceil(4.9);     //5 larger interger value
+
+Math.random()  ;     //value b/w 0 and 1 //not 1
+
+
+// generate random number b/w 1 and 10 
+
+// let num = Math.random();
+
+// num = num * 10;
+
+// num = Math.floor(num);
+
+Math.floor(Math.random()*10) +1; 
+
+
+
+
