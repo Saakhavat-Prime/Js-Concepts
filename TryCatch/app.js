b18b72ee14c4f2e2921792e2b93d045008542a87
Saakhavat-Prime <sakhavathussayn@gmail.com>
2026-06-20 16:08:50 +0500

    trycatch

---
+++ TryCatch/app.js
@@ -0,0 +1,11 @@
+console.log("hello")
+console.log("hello")
+// a = 5;
+try{
+console.log(a);
+}catch(err){
+    console.log("a is not define");
+    console.log(err)
+}
+console.log("hello")
+console.log("hello")
