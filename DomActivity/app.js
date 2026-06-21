2856c06dbdc0b352d74d827a8fc51c80b32a61fd
Saakhavat-Prime <sakhavathussayn@gmail.com>
2026-06-21 21:55:46 +0500

    DomActivity

---
+++ DomActivity/app.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+
+    let h1 =document.querySelector('h1');
+    let bgc = randomColor();
+    h1.innerText = bgc;
+    
+    let div = document.querySelector('div');
+        
+        div.style.backgroundColor = bgc; 
+    
+    
+    
+
+})
+
+let randomColor = ()=>{
+   let red =  Math.floor(Math.random() * 255);
+   let green =  Math.floor(Math.random() * 255);
+   let blue =  Math.floor(Math.random() * 255);
+    
+   let color = `rgb(${red},${green},${blue})`;
+   return color;
+}
